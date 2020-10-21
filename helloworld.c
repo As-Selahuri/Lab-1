8f17e1be6c6e7bf6aba0ffee48e9150446804a9c
Muhammad Izwan <2019872166@isiswa.uitm.edu.my>
2020-10-21 03:11:22 +0000

    'My First commit

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/*This is my first program in C*/
+int main(void){
+printf("Hello World!\n");
+printf("I love C\n");
+return (0);
+}
---
+++ userinput.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+/*This is my second proram in C*/
+int age;
+printf("Hi, how old are you?>\n");
+scanf("%d", &age);
+printf("You are %d years old",age);
+return(0);
+}
